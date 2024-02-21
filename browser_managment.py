1dfcb73f45fd0ebd0bedca85fe41f2b0cf38ce98
Doson <karbushev-1991@mail.ru>
2024-02-21 23:19:51 +0700

    Добавил ожидание в click_open_profile, добавил функцию click_no

--- browser_managment.py
+++ browser_managment.py
@@ -5,6 +5,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def click_recommendations(driver: WebDriver) -> None:
@@ -18,18 +20,32 @@ def click_recommendations(driver: WebDriver) -> None:
 # нажатие кнопки "Открыть профиль"
 def click_open_profile(driver: WebDriver):
     try:
-        driver.find_elements(By.XPATH, '//div/div[@role="button"]')[1].send_keys(Keys.ENTER)
+        wait = WebDriverWait(driver, 10)  # wait for up to 10 seconds
+        buttons = wait.until(EC.presence_of_all_elements_located((By.XPATH, '//div/div[@role="button"]')))
+        buttons[1].send_keys(Keys.ENTER)
+        logger.info('Кнопка "Открыть профиль" нажата')
     except Exception as ex:
-        logger.error('Open profile button not found')
+        logger.error(f'Open profile button not found: {ex}')
 
 
-def click_slider(driver: WebDriver, name):
+def get_sliders(driver: WebDriver, name):
     try:
-        slider = driver.find_elements(
+        sliders = driver.find_elements(
             By.XPATH, f'//div[@role="tablist" and contains(@aria-label,"{name}")]/button')
-        slider.click()
-        time.sleep(1)
-        slider.send_keys(Keys.ARROW_RIGHT)
+        return sliders
     except Exception as ex:
         logger.error('Slider not found')
+
+
+# клик по кнопке Нет
+def click_no(driver: WebDriver):
+    # Xpath для кнопки NO не заходя в профиль
+    #//button[@class="button Lts($ls-s) Z(0) CenterAlign Mx(a) Cur(p) Tt(u) Bdrs(50%) P(0) Fw($semibold) focus-button-style Bxsh($bxsh-btn) Expand Trstf(e) Trsdu($normal) Wc($transform) Pe(a) Scale(1.1):h Scale(.9):a Bgi($g-ds-background-nope):a"]
+    try:
+        (driver.find_element(By.XPATH,
+            '//button[@class="button Lts($ls-s) Z(0) CenterAlign Mx(a) Cur(p) Tt(u) Bdrs(50%) P(0) Fw($semibold) focus-button-style Bxsh($bxsh-btn) Expand Trstf(e) Trsdu($normal) Wc($transform) Pe(a) Scale(1.1):h Scale(.9):a Bgc($c-ds-background-primary)"]')
+         .send_keys(Keys.ENTER))
+        logger.info('Кнопка "Нет" нажата')
+    except Exception as ex:
+        logger.error('No button not found')
 
